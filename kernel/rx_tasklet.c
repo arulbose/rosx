f6d1c3e117f461b80ce8ce56aa1aa078516ec554
Arul <bose.arul@gmail.com>
2018-11-20 10:03:52 +0530

    added tasklet functions

--- kernel/rx_tasklet.c
+++ kernel/rx_tasklet.c
@@ -17,18 +17,55 @@
 
 #include <RosX.h>
 
+struct tasklet * sys_tasklet_list = NULL;
 /* Under construction */
 
-int rx_create_tasklet(struct tasklet *t, void(*func)(unsigned long), unsigned long data);
-void rx_delete_tasklet(struct tasklet *);
-void rx_enable_tasklet(struct tasklet *);
-void rx_disable_tasklet(struct tasklet *);
-void rx_schedule_tasklet(struct tasklet *);
+int rx_create_tasklet(struct tasklet *t, void(*func)(unsigned long), unsigned long data)
+{
+    if((t == NULL) || (func == NULL) ){
+        return OS_ERR;
+    }
+
+    t->func = func;
+    t->data = data;
+    t->next = NULL;
+  
+    return OS_OK;
+
+}
+
+void rx_delete_tasklet(struct tasklet *t)
+{
+
+}
+
+void rx_enable_tasklet(struct tasklet *t)
+{
+
+}
+
+void rx_disable_tasklet(struct tasklet *t)
+{
+
+}
+
+void rx_schedule_tasklet(struct tasklet *t)
+{
+
+}
 
 void rx_bh_thread()
 {
+    RX_DEFINE_WAITQUEUE(w);
+
     while(1) {
-        rx_suspend_task(MYSELF);
+
+         if(0 == rx_wait_queue(&w, (sys_tasklet_list != NULL))) {
+
+         /* Process the tasklet serially */
+
+
+         }
+
     }
-    
 }
